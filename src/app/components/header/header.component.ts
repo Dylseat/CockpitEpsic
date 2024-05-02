2b3230354682941406b80439db9e31cdd15c9c4d
Dylseat <dyl0301@protonmail.com>
2024-05-02 08:42:29 +0200

    add icon Logout

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
 import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [MatButtonModule, MatToolbarModule],
+  imports: [MatButtonModule, MatToolbarModule, MatIconModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
@@ -24,6 +25,10 @@ export class HeaderComponent {
 
   navigateNote() {
     this.router.navigate(['/note']);
+  }
+
+  navigateLogin() {
+    this.router.navigate(['/login']);
   }
   
 }
