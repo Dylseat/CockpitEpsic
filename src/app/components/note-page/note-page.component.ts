9ca59335be4afda3964f30bb870edd2c1b4c4fe6
ducry <fabien.ducry@formation.orif.ch>
2024-05-03 13:36:23 +0200

    Test des tableaux + ajout de la db + correction bug header

--- src/app/components/note-page/note-page.component.ts
+++ src/app/components/note-page/note-page.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
 import {MatTabsModule} from '@angular/material/tabs';
+import {Notes1BranchesTableComponent} from '../notes1-branches-table/notes1-branches-table.component';
+
 
 @Component({
   selector: 'app-note-page',
   standalone: true,
-  imports: [HeaderComponent, MatTabsModule],
+  imports: [HeaderComponent, MatTabsModule, Notes1BranchesTableComponent],
   templateUrl: './note-page.component.html',
   styleUrl: './note-page.component.scss'
 })
