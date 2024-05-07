397024729fc1062ac40130e0c2fd45ca1014ae8f
Dylseat <dyl0301@protonmail.com>
2024-05-07 13:38:20 +0200

    Update route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,7 +2,7 @@ import { Routes } from '@angular/router';
 import { LoginPageComponent } from './components/login-page/login-page.component';
 import { HomePageComponent } from './components/home-page/home-page.component'; 
 import { NotePageComponent } from './components/note-page/note-page.component'; 
-import { PageHoraireComponent } from './page-horaire/page-horaire.component';
+import { PageHoraireComponent } from './components/page-horaire/page-horaire.component';
 
 export const routes: Routes = [ 
     { path: '', redirectTo: '/login', pathMatch: 'full' },  // Redirige l'URL de base vers /login
