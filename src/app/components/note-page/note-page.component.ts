ba204d12a0e0b753a3d9940168a9debbb0f316b6
Annie Cuendet <anniecuendet@macbook-pro-de-annie.home>
2024-05-09 17:13:17 +0200

    modification du tableau des notes

--- src/app/components/note-page/note-page.component.ts
+++ src/app/components/note-page/note-page.component.ts
@@ -1,17 +1,17 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
-import {MatTabsModule} from '@angular/material/tabs';
-import {Notes1BranchesTableComponent} from '../notes1-branches-table/notes1-branches-table.component';
-import {Notes1ModulesTableComponent} from '../notes1-modules-table/notes1-modules-table.component';
+import { MatTabsModule } from '@angular/material/tabs';
+import { Notes1BranchesTableComponent } from '../notes1-branches-table/notes1-branches-table.component';
+import { Notes1ModulesTableComponent } from '../notes1-modules-table/notes1-modules-table.component';
 
 
 @Component({
-  selector: 'app-note-page',
-  standalone: true,
-  imports: [HeaderComponent, MatTabsModule, Notes1BranchesTableComponent, Notes1ModulesTableComponent],
-  templateUrl: './note-page.component.html',
-  styleUrl: './note-page.component.scss'
+	selector: 'app-note-page',
+	standalone: true,
+	imports: [HeaderComponent, MatTabsModule, Notes1BranchesTableComponent, Notes1ModulesTableComponent],
+	templateUrl: './note-page.component.html',
+	styleUrls: ['./note-page.component.scss']
 })
 export class NotePageComponent {
-
+	
 }
